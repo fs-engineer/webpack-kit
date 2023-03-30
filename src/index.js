bfc71796469f83c35a29ffad2372dbb9afdcae00
Vadym Yevlanov <vad.evlanov@gmail.com>
2023-03-30 19:03:59 +0200

    added loaders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import './main.scss';
+
 function component(text) {
-    const element = document.createElement('h1');
-    element.textContent = text;
+    const element = document.createElement('h2');
+
+    // Lodash, now imported by this script
+    element.innerHTML = text;
+
     return element;
 }
 
-document.body.prepend(component('Проект собран на Webpack'));
+document.body.prepend(component('Проект собран на Webpack 5'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const FileManagerPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     // mode: "development",
@@ -13,15 +15,31 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'template.html'),
+            template: path.join(__dirname, 'src', 'index.html'),
             filename: 'index.html',
         }),
         new FileManagerPlugin({
@@ -31,9 +49,29 @@ module.exports = {
                 },
             },
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
     ],
     devServer: {
          watchFiles: path.join(__dirname, 'src'),
          port: 3377,
+    },
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            ['optipng', { optimizationLevel: 5 }],
+                            ['svgo', { name: 'preset-default' }],
+                        ],
+                    },
+                },
+            }),
+        ],
     },
 };
